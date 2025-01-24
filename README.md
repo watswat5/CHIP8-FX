fbf88a6904804b954ff3d828e42b720b33611520
Chris Antepenko <ca96256@uga.edu>
2025-01-23 19:17:31 -0500

    Fixed run.sh to work with Maven

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A CHIP-8 interpreter running in Java, using JavaFX as the graphical frontend.
+
+Build with "mvn package"
+Requires javafx to be installed. You can use OpenJFX (https://openjfx.io/)
+run.sh will only work if JavaFX is in /usr/share/openjfx/lib
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -cp bin -p /home/chris/Documents/JFX/javafx-sdk-22/lib/ --add-modules javafx.controls chip8.base.Driver
+java --module-path "/usr/share/openjfx/lib" --add-modules javafx.controls,javafx.graphics -cp target/chip8-base-1.0-SNAPSHOT.jar chip8.base.Driver
