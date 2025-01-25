d83b3d255f540772147dd4b8c59808fab3b8dbe0
Christopher Antepenko <ca96256@uga.edu>
2025-01-24 23:34:55 -0500

    Removed outdated build scripts

--- compile.sh
+++
@@ -1,2 +0,0 @@
-javac -d bin -cp bin -p /home/chris/Documents/JFX/javafx-sdk-22/lib/ --add-modules javafx.controls src/chip8/base/CHIP8.java
-javac -d bin -cp bin -p /home/chris/Documents/JFX/javafx-sdk-22/lib/ --add-modules javafx.controls src/chip8/base/Driver.java
